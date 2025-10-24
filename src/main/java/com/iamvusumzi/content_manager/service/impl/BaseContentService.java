c4ccb1c7b4fb4aaa69d56c445f8c452149a50548
Vusumzi Msengana <vmsengana2@gmail.com>
2025-10-24 23:58:09 +0200

    test(content): add unit and controller tests for user/admin services

--- src/main/java/com/iamvusumzi/content_manager/service/impl/BaseContentService.java
+++ src/main/java/com/iamvusumzi/content_manager/service/impl/BaseContentService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 public abstract class BaseContentService {
     protected final ContentRepository contentRepository;
-    protected final UserRepository  userRepository;
+    protected final UserRepository userRepository;
 
     protected BaseContentService(ContentRepository contentRepository, UserRepository userRepository) {
         this.contentRepository = contentRepository;
@@ -37,9 +37,9 @@ public abstract class BaseContentService {
         return contentRepository.save(content);
     }
 
-    protected List<Content> findContentByAuthor(String username){
+    protected List<Content> findContentByAuthor(String username) {
         User author = userRepository.findByUsername(username)
-                .orElseThrow(()-> new RuntimeException("User not found: " + username));
+                .orElseThrow(() -> new RuntimeException("User not found: " + username));
 
         return contentRepository.findByAuthorIdOrderByDateCreatedDesc(author.getId());
     }
@@ -47,16 +47,22 @@ public abstract class BaseContentService {
     protected Content updateContentCommon(String username, Integer contentId, ContentRequest request) {
 
         User currentUser = userRepository.findByUsername(username)
-                .orElseThrow(()-> new RuntimeException("User not found"));
+                .orElseThrow(() -> new RuntimeException("User not found"));
         Content existing = contentRepository.findById(contentId)
-                .orElseThrow(()-> new RuntimeException("Content not found"));
+                .orElseThrow(() -> new RuntimeException("Content not found"));
         boolean isAuthor = existing.getAuthor().getId().equals(currentUser.getId());
 
-        if(!isAuthor) throw new AccessDeniedException("You are not authorized to update this content");
+        if (!isAuthor) throw new AccessDeniedException("You are not authorized to update this content");
 
-        if (request.getTitle() != null) { existing.setTitle(request.getTitle()); }
-        if (request.getDesc() != null) { existing.setDesc(request.getDesc()); }
-        if (request.getStatus() != null) {existing.setStatus(Status.valueOf(request.getStatus().toUpperCase())); }
+        if (request.getTitle() != null) {
+            existing.setTitle(request.getTitle());
+        }
+        if (request.getDesc() != null) {
+            existing.setDesc(request.getDesc());
+        }
+        if (request.getStatus() != null) {
+            existing.setStatus(Status.valueOf(request.getStatus().toUpperCase()));
+        }
 
         existing.setDateUpdated(LocalDateTime.now());
 
